d20ac648e4bc6aa48634517a47833514da99d6e4
JuanVimon3 <jvillamonsalve@gmail.com>
2026-02-21 16:46:44 -0500

    add react-swipeable to modal list

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -48,6 +48,15 @@ export default function ExpenseForm() {
    //Agregar un nuevo gasto
 
    dispatch({type: 'add-expense', payload: {expense}})
+
+   //Reiniciar state
+
+   setExpense({
+    amount: 0,
+    expenseName: '',
+    category: '',
+    date: new Date()
+   })
   }
 
   return (
